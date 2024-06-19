96551b6f55661a681e017c70003a4fe15c8d0800
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-06-19 22:07:30 +0200

    Implemented a grid layout using flexbox

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ let resetBtn = document.querySelector(".reset"),
 resetBtn.addEventListener('click', resetBoard);
 
 function resetBoard() {
-    let size = Number.parseInt(prompt("Choose pad resolution (1-100)")),
-        dimensions = size * size;
+    let size = Number.parseInt(prompt("Choose pad resolution (1-100)"));
     
     pad.innerHTML = "";
 
-    for (let index = 0; index < dimensions; index++) {
-        let pixel = document.createElement("div")
-        pixel.classList.add("pixel");
-        pixel.textContent = index
-        pad.appendChild(pixel);
+    for (let index = 0; index < size; index++) {
+        let pixelRow = document.createElement("div");
+        pixelRow.classList.add("pixel-row");
+
+        let dimensions = 100/size;
+        pixelRow.setAttribute("height", dimensions+"%");
+
+        for (let i = 0; i < size; i++) {
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            pixel.setAttribute("width", dimensions+"");
+
+            pixelRow.appendChild(pixel);         
+        }
+
+        pad.appendChild(pixelRow);
     }
 }
 
